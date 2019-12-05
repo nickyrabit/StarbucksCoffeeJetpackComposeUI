7e42e6010264fefdfc92278cd0318b9f27306fcf
Nicky Ngailo <nickyrabit@gmail.com>
2019-12-05 14:39:11 +0300

    removing unused resources

--- app/src/main/java/com/nickyrabit/starbucks/MainActivity.kt
+++ app/src/main/java/com/nickyrabit/starbucks/MainActivity.kt
@@ -2,26 +2,8 @@ package com.nickyrabit.starbucks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.ui.core.*
-import androidx.ui.foundation.ColoredRect
-import androidx.ui.foundation.DrawImage
-import androidx.ui.foundation.HorizontalScroller
-import androidx.ui.foundation.VerticalScroller
-import androidx.ui.foundation.shape.DrawShape
-import androidx.ui.foundation.shape.corner.CircleShape
-import androidx.ui.foundation.shape.corner.RoundedCornerShape
-import androidx.ui.graphics.Color
-import androidx.ui.layout.*
+import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.surface.Card
-import androidx.ui.material.withOpacity
-import androidx.ui.text.TextStyle
-import androidx.ui.text.font.Font
-import androidx.ui.text.font.FontFamily
-import androidx.ui.text.font.FontWeight
-import androidx.ui.tooling.preview.Preview
-import com.nickyrabit.starbucks.images.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
