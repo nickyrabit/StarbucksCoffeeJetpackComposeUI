0493eadbecbb8f7f05cbc60009d0df200ea05cf2
Nicky Ngailo <nickyrabit@gmail.com>
2019-12-05 15:29:06 +0300

    adding comment

--- app/src/main/java/com/nickyrabit/starbucks/MainActivity.kt
+++ app/src/main/java/com/nickyrabit/starbucks/MainActivity.kt
@@ -4,7 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
-
+/**
+ *
+ * Nicholaus Legnard
+ *
+ *  (+255) 685 097 713
+ *
+ *  nickyrabit@gmail.com
+ *
+ *  nicky@nickylegnard.com
+ *
+ *  Instagram : @nicktabit
+ *  Twitter : @nngailo
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
